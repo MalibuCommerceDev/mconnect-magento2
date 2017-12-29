cb2aa278f2bd1db29d28b90d524aee7fd46639f7
Andrey Babich <andrey.babich@gmail.com>
2017-12-29 23:49:41 +0200

    MALC-54 - M2 Mconnect: Implement Orders grid and Customers Grid NAV sync status, update Customers and Order Grids.

--- Ui/Component/Listing/Column/CustomerMConnectStatus.php
+++ Ui/Component/Listing/Column/CustomerMConnectStatus.php
@@ -2,6 +2,7 @@
 
 namespace MalibuCommerce\MConnect\Ui\Component\Listing\Column;
 
+use \MalibuCommerce\MConnect\Model\Queue;
 use \Magento\Customer\Api\Data\CustomerInterface;
 use \Magento\Framework\View\Element\UiComponent\ContextInterface;
 use \Magento\Framework\View\Element\UiComponentFactory;
@@ -9,13 +10,16 @@ use \Magento\Ui\Component\Listing\Columns\Column;
 
 class CustomerMConnectStatus extends \Magento\Ui\Component\Listing\Columns\Column
 {
+    protected $_queue;
 
     public function __construct(
         ContextInterface $context,
         UiComponentFactory $uiComponentFactory,
+        Queue $queue,
         array $components = [],
         array $data = [])
     {
+        $this->_queue = $queue;
         parent::__construct($context, $uiComponentFactory, $components, $data);
     }
 
@@ -24,22 +28,28 @@ class CustomerMConnectStatus extends \Magento\Ui\Component\Listing\Columns\Colum
         if (isset($dataSource['data']['items'])) {
             foreach ($dataSource['data']['items'] as & $item) {
 
-                $status = $item['mconnect_status'];
+                $queueCollection = $this->_queue->getCollection()
+                    ->addFilter('code', 'customer')
+                    ->addFilter('entity_id', $item['entity_id'])
+                    ->setOrder('finished_at', 'desc');
+                $queue = $queueCollection->getFirstItem();
+
+                $status = $queue->getData('status');
                 $navId = $item['nav_id'];
 
                 switch ($status) {
-                    case "0":
-                        $export_status = '<span title="" style="text-transform: uppercase; font-weight: bold; color: white; font-size: 10px; width: 100%; display: block; text-align: center; border-radius: 10px; background: #ff0000;">error</span>';
+                    case "error":
+                        $mConnectStatus = '<span title="" style="text-transform: uppercase; font-weight: bold; color: white; font-size: 10px; width: 100%; display: block; text-align: center; border-radius: 10px; background: #ff0000;">error</span>';
                         break;
-                    case "1";
-                        $export_status = '<span title="Order exported, NAV ID: '. $navId .'" style="text-transform: uppercase; font-weight: bold; color: white; font-size: 10px; width: 100%; display: block; text-align: center; border-radius: 10px; background: #00c500;">success</span>';
+                    case "success";
+                        $mConnectStatus = '<span title="Order exported, NAV ID: '. $navId .'" style="text-transform: uppercase; font-weight: bold; color: white; font-size: 10px; width: 100%; display: block; text-align: center; border-radius: 10px; background: #00c500;">success</span>';
                         break;
                     default:
-                        $export_status = '';
+                        $mConnectStatus = '';
                         break;
                 }
 
-                $item[$this->getData('name')] = $export_status;
+                $item[$this->getData('name')] = $mConnectStatus;
             }
         }
 
--- Ui/Component/Listing/Column/OrderMConnectStatus.php
+++ Ui/Component/Listing/Column/OrderMConnectStatus.php
@@ -3,6 +3,7 @@
 namespace MalibuCommerce\MConnect\Ui\Component\Listing\Column;
 
 use \Magento\Sales\Api\OrderRepositoryInterface;
+use \MalibuCommerce\MConnect\Model\Queue;
 use \Magento\Framework\View\Element\UiComponent\ContextInterface;
 use \Magento\Framework\View\Element\UiComponentFactory;
 use \Magento\Ui\Component\Listing\Columns\Column;
@@ -12,14 +13,18 @@ class OrderMConnectStatus extends \Magento\Ui\Component\Listing\Columns\Column
 {
     protected $_orderRepository;
 
+    protected $_queue;
+
     public function __construct(
         ContextInterface $context,
         UiComponentFactory $uiComponentFactory,
         OrderRepositoryInterface $orderRepository,
+        Queue $queue,
         array $components = [],
         array $data = [])
     {
         $this->_orderRepository = $orderRepository;
+        $this->_queue = $queue;
         parent::__construct($context, $uiComponentFactory, $components, $data);
     }
 
@@ -28,26 +33,29 @@ class OrderMConnectStatus extends \Magento\Ui\Component\Listing\Columns\Column
         if (isset($dataSource['data']['items'])) {
             foreach ($dataSource['data']['items'] as & $item) {
 
+                $queueCollection = $this->_queue->getCollection()
+                    ->addFilter('code', 'order')
+                    ->addFilter('entity_id', $item['entity_id'])
+                    ->setOrder('finished_at', 'desc');
+                $queue = $queueCollection->getFirstItem();
+                $status = $queue->getData('status');
+
                 $order  = $this->_orderRepository->get($item["entity_id"]);
-                $status = $order->getData('mconnect_status');
                 $navId = $order->getData('nav_id');
 
                 switch ($status) {
-                    case "0":
-                        //$export_status = "No";
-                        $export_status = '<span title="" style="text-transform: uppercase; font-weight: bold; color: white; font-size: 10px; width: 100%; display: block; text-align: center; border-radius: 10px; background: #ff0000;">error</span>';
+                    case "error":
+                        $mConnectStatus = '<span title="" style="text-transform: uppercase; font-weight: bold; color: white; font-size: 10px; width: 100%; display: block; text-align: center; border-radius: 10px; background: #ff0000;">error</span>';
                         break;
-                    case "1";
-                        //$export_status = "Yes";
-                        $export_status = '<span title="Order exported, NAV ID: '. $navId .'" style="text-transform: uppercase; font-weight: bold; color: white; font-size: 10px; width: 100%; display: block; text-align: center; border-radius: 10px; background: #00c500;">success</span>';
+                    case "success";
+                        $mConnectStatus = '<span title="Order exported, NAV ID: '. $navId .'" style="text-transform: uppercase; font-weight: bold; color: white; font-size: 10px; width: 100%; display: block; text-align: center; border-radius: 10px; background: #00c500;">success</span>';
                         break;
                     default:
-                        //$export_status = "Failed";
-                        $export_status = '';
+                        $mConnectStatus = '';
                         break;
                 }
 
-                $item[$this->getData('name')] = $export_status;
+                $item[$this->getData('name')] = $mConnectStatus;
             }
         }
 
