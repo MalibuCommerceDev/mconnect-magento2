4775b48202aa506bcf5a7bf3577fdca3bd8583f9
Andrey Babich <andrey.babich@gmail.com>
2018-01-04 16:31:19 +0200

    MALC-56 - Clean up module from M1 code that was auto imported but not in use, delete unused files.

--- Block/Adminhtml/System/Config/Customer/Lastsync.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace MalibuCommerce\MConnect\Block\Adminhtml\System\Config\Customer;
-
-
-class Lastsync
-    extends \MalibuCommerce\MConnect\Block\Adminhtml\System\Config\Lastsync
-{
-    protected $_code = 'customer';
-}
