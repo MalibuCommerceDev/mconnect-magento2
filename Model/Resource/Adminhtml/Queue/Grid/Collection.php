08989fce80a627bf9c55c33bdd094ef0a598c405
Maksym Saveliev <maks@frendino.com>
2018-03-09 03:56:35 -0500

    Fixed MALC-141 [Malibu Commerce] Mconnect M2: MC20-61 Sync grid issue when paginating

--- Model/Resource/Adminhtml/Queue/Grid/Collection.php
+++ Model/Resource/Adminhtml/Queue/Grid/Collection.php
@@ -2,49 +2,35 @@
 
 namespace MalibuCommerce\MConnect\Model\Resource\Adminhtml\Queue\Grid;
 
-class Collection extends \MalibuCommerce\MConnect\Model\Resource\Adminhtml\Queue\Collection
-    implements \Magento\Framework\Api\Search\SearchResultInterface
+use Magento\Framework\Data\Collection\Db\FetchStrategyInterface as FetchStrategy;
+use Magento\Framework\Data\Collection\EntityFactoryInterface as EntityFactory;
+use Magento\Framework\Event\ManagerInterface as EventManager;
+use Psr\Log\LoggerInterface as Logger;
+
+/**
+ * Queue grid collection
+ */
+class Collection extends \Magento\Framework\View\Element\UiComponent\DataProvider\SearchResult
 {
     /**
-     * Aggregations
+     * Initialize dependencies.
      *
-     * @var \Magento\Framework\Search\AggregationInterface
-     */
-    protected $_aggregations;
-
-    /**
-     * constructor
-     *
-     * @param \Magento\Framework\Data\Collection\EntityFactoryInterface    $entityFactory
-     * @param \Psr\Log\LoggerInterface                                     $logger
-     * @param \Magento\Framework\Data\Collection\Db\FetchStrategyInterface $fetchStrategy
-     * @param \Magento\Framework\Event\ManagerInterface                    $eventManager
-     * @param                                                              $mainTable
-     * @param                                                              $eventPrefix
-     * @param                                                              $eventObject
-     * @param                                                              $resourceModel
-     * @param                                                              $model
-     * @param                                                              $connection
-     * @param \Magento\Framework\Model\ResourceModel\Db\AbstractDb         $resource
+     * @param EntityFactory $entityFactory
+     * @param Logger $logger
+     * @param FetchStrategy $fetchStrategy
+     * @param EventManager $eventManager
+     * @param string $mainTable
+     * @param string $resourceModel
      */
     public function __construct(
-        \Magento\Framework\Data\Collection\EntityFactoryInterface $entityFactory,
-        \Psr\Log\LoggerInterface $logger,
-        \Magento\Framework\Data\Collection\Db\FetchStrategyInterface $fetchStrategy,
-        \Magento\Framework\Event\ManagerInterface $eventManager,
-        $mainTable,
-        $eventPrefix,
-        $eventObject,
-        $resourceModel,
-        $model = 'Magento\Framework\View\Element\UiComponent\DataProvider\Document',
-        \Magento\Framework\DB\Adapter\AdapterInterface $connection = null,
-        \Magento\Framework\Model\ResourceModel\Db\AbstractDb $resource = null
+        EntityFactory $entityFactory,
+        Logger $logger,
+        FetchStrategy $fetchStrategy,
+        EventManager $eventManager,
+        $mainTable = 'malibucommerce_mconnect_queue',
+        $resourceModel = \MalibuCommerce\MConnect\Model\Resource\Adminhtml\Queue::class
     ) {
-        parent::__construct($entityFactory, $logger, $fetchStrategy, $eventManager, $connection, $resource);
-        $this->_eventPrefix = $eventPrefix;
-        $this->_eventObject = $eventObject;
-        $this->_init($model, $resourceModel);
-        $this->setMainTable($mainTable);
+        parent::__construct($entityFactory, $logger, $fetchStrategy, $eventManager, $mainTable, $resourceModel);
     }
 
     /**
@@ -58,88 +44,9 @@ class Collection extends \MalibuCommerce\MConnect\Model\Resource\Adminhtml\Queue
 
         $this->getSelect()
             ->columns(array(
-                'duration' => 'IF(main_table.finished_at, TIME_TO_SEC(TIMEDIFF(main_table.finished_at, main_table.started_at)), "")')
+                    'duration' => 'IF(main_table.finished_at, TIME_TO_SEC(TIMEDIFF(main_table.finished_at, main_table.started_at)), "")')
             );
 
-        return $this;
-    }
-
-    /**
-     * @return \Magento\Framework\Search\AggregationInterface
-     */
-    public function getAggregations()
-    {
-        return $this->_aggregations;
-    }
-
-    /**
-     * @param \Magento\Framework\Search\AggregationInterface $aggregations
-     *
-     * @return $this
-     */
-    public function setAggregations($aggregations)
-    {
-        $this->_aggregations = $aggregations;
-
-        return $this;
-    }
-
-    /**
-     * Get search criteria.
-     *
-     * @return \Magento\Framework\Api\SearchCriteriaInterface|null
-     */
-    public function getSearchCriteria()
-    {
-        return null;
-    }
-
-    /**
-     * Set search criteria.
-     *
-     * @param \Magento\Framework\Api\SearchCriteriaInterface $searchCriteria
-     *
-     * @return $this
-     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
-     */
-    public function setSearchCriteria(\Magento\Framework\Api\SearchCriteriaInterface $searchCriteria = null)
-    {
-        return $this;
-    }
-
-    /**
-     * Get total count.
-     *
-     * @return int
-     */
-    public function getTotalCount()
-    {
-        return $this->getSize();
-    }
-
-    /**
-     * Set total count.
-     *
-     * @param int $totalCount
-     *
-     * @return $this
-     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
-     */
-    public function setTotalCount($totalCount)
-    {
-        return $this;
-    }
-
-    /**
-     * Set items list.
-     *
-     * @param \Magento\Framework\Api\ExtensibleDataInterface[] $items
-     *
-     * @return $this
-     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
-     */
-    public function setItems(array $items = null)
-    {
         return $this;
     }
 }
