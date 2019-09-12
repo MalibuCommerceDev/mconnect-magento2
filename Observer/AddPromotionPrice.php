680b956794577dc83772ea5a707f538e0fdea4e8
Dima <ladonenko@gmail.com>
2019-09-12 23:15:55 +0300

    MALC-320

--- Observer/AddPromotionPrice.php
+++ Observer/AddPromotionPrice.php
@@ -25,6 +25,8 @@ class AddPromotionPrice implements \Magento\Framework\Event\ObserverInterface
      */
     protected $storeManager;
 
+    protected $prepareProducts = [];
+
     /**
      * PromotionPlugin constructor.
      *
@@ -54,46 +56,51 @@ class AddPromotionPrice implements \Magento\Framework\Event\ObserverInterface
         /* @var $collection \Magento\Catalog\Model\ResourceModel\Product\Collection */
         if ($collection->getSize() > 0 ) {
             $key = \MalibuCommerce\MConnect\Model\Queue\Promotion::REGISTRY_KEY_NAV_PROMO_PRODUCTS;
-            $prepareProducts = $this->registry->registry($key);
-            $prepareProducts = [];
+            $simpleProducts = [];
             foreach ($collection as $product) {
-                $prepareProducts[$product->getSku()] = self::PRODUCT_QTY_FOR_IMPORT;
-
+                $this->prepareProducts[$product->getSku()] = self::PRODUCT_QTY_FOR_IMPORT;
                 if ($product->getTypeId() == \Magento\ConfigurableProduct\Model\Product\Type\Configurable::TYPE_CODE) {
-                    //$simpleProducts = $this->linkManagement->getChildren($product->getSku());
-                    $connection = $this->resourceConnection->getConnection();
-                    $select = $connection->select();
-                    $select->from(
-                        ['product' => $connection->getTableName('catalog_product_entity')],
-                        ['entity_id', 'sku']
-                    );
-                    $select->joinInner(
-                        ['link_table' => 'catalog_product_super_link'],
-                        'product.entity_id = link_table.product_id',
-                        []
-                    );
-                    $select->where('link_table.parent_id IN (?)', [$product->getId()]);
-
-                    $simpleProducts = $connection->fetchAssoc($select);
-                    if (count($simpleProducts) > 0) {
-                        foreach ($simpleProducts as $simpleProduct) {
-                            $prepareProducts[$simpleProduct['sku']] = self::PRODUCT_QTY_FOR_IMPORT;
-                        }
-                    }
+                    $simpleProducts[] = $product->getId();
                 }
             }
+            $this->loadSimpleProducts($simpleProducts);
             $this->registry->unregister($key);
-            $this->registry->register($key, $prepareProducts);
+            $this->registry->register($key, $this->prepareProducts);
 
             $store = $this->storeManager->getStore($collection->getStoreId());
             $websiteId = $store->getWebsiteId();
             $this->promotion->runMultiplePromoPriceImport($websiteId);
-            foreach ($prepareProducts as $itemKey => $itemValue) {
+
+            //Save products without promo price to cache
+            foreach ($this->prepareProducts as $itemKey => $itemValue) {
                 if (!$this->promotion->getPriceFromCache($itemKey, self::PRODUCT_QTY_FOR_IMPORT)) {
                     $this->promotion->savePromoPriceToCache(['price' => 'NULL', 'quantity' => self::PRODUCT_QTY_FOR_IMPORT], $itemKey, $websiteId);
                 }
             }
         }
         return $this;
+    }
+
+    public function loadSimpleProducts($simpleProducts)
+    {
+        $connection = $this->resourceConnection->getConnection();
+        $select = $connection->select();
+        $select->from(
+            ['product' => $connection->getTableName('catalog_product_entity')],
+            ['entity_id', 'sku']
+        );
+        $select->joinInner(
+            ['link_table' => 'catalog_product_super_link'],
+            'product.entity_id = link_table.product_id',
+            []
+        );
+        $select->where('link_table.parent_id IN (?)', $simpleProducts);
+
+        $simpleProducts = $connection->fetchAssoc($select);
+        if (count($simpleProducts) > 0) {
+            foreach ($simpleProducts as $simpleProduct) {
+                $this->prepareProducts[$simpleProduct['sku']] = self::PRODUCT_QTY_FOR_IMPORT;
+            }
+        }
     }
 }
