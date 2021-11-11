e0c9ed9fc9673c86a23277cb5a34f0ca171b97db
sofvlad <sofienkovladislav@gmail.com>
2021-11-11 16:49:32 +0600

    Implemented MALC-483: [West Music] WEST-16 Sync item Unit Price as additional element with Inventory sync
    - Implemented support <unit_price> XML value to update product price

--- Model/Queue/Inventory.php
+++ Model/Queue/Inventory.php
@@ -1,10 +1,19 @@
 <?php
 
+declare(strict_types=1);
+
 namespace MalibuCommerce\MConnect\Model\Queue;
 
 use Magento\Catalog\Api\Data\ProductInterface;
+use Magento\Catalog\Api\ProductRepositoryInterface;
+use Magento\CatalogInventory\Api\Data\StockItemInterface;
 use Magento\CatalogInventory\Api\StockConfigurationInterface;
 use Magento\Framework\App\ProductMetadataInterface;
+use Magento\Framework\DataObject;
+use Magento\Framework\Exception\CouldNotSaveException;
+use Magento\Framework\Exception\InputException;
+use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Framework\Exception\StateException;
 use Magento\Inventory\Model\ResourceModel\Source\Collection as SourceCollection;
 use Magento\Inventory\Model\ResourceModel\Source\CollectionFactory as SourceCollectionFactory;
 use Magento\InventoryApi\Api\Data\SourceInterface;
@@ -12,36 +21,25 @@ use Magento\InventoryApi\Api\Data\SourceItemInterfaceFactory;
 use Magento\InventoryCatalogApi\Api\DefaultSourceProviderInterface;
 use Magento\InventoryCatalogApi\Model\IsSingleSourceModeInterface;
 use Magento\InventorySalesApi\Api\Data\SalesChannelInterface;
+use MalibuCommerce\MConnect\Model\Config;
+use MalibuCommerce\MConnect\Model\Navision\Inventory as NavisionInventory;
+use MalibuCommerce\MConnect\Model\Queue;
+use MalibuCommerce\MConnect\Model\Queue\Inventory\SourceItemsProcessor;
 
-class Inventory extends \MalibuCommerce\MConnect\Model\Queue implements ImportableEntity
+class Inventory extends Queue implements ImportableEntity
 {
     const CODE = 'inventory';
     const NAV_XML_NODE_ITEM_NAME = 'item_inventory';
 
     /**
-     * @var \Magento\Catalog\Api\ProductRepositoryInterface|ProductRepositoryInterface
-     */
-    protected $productRepository;
-
-    /**
-     * @var \MalibuCommerce\MConnect\Model\Navision\Product|Inventory
-     */
-    protected $navInventory;
-
-    /**
-     * @var \MalibuCommerce\MConnect\Model\Config|Config
+     * @var Config
      */
     protected $config;
 
     /**
-     * @var \MalibuCommerce\MConnect\Model\Queue\FlagFactory
-     */
-    protected $queueFlagFactory;
-
-    /**
      * @var StockConfigurationInterface
      */
-    protected $configuration;
+    protected $stockConfiguration;
 
     /**
      * @var DefaultSourceProviderInterface
@@ -54,9 +52,24 @@ class Inventory extends \MalibuCommerce\MConnect\Model\Queue implements Importab
     protected $isSingleSourceMode;
 
     /**
+     * @var NavisionInventory
+     */
+    protected $navisionInventory;
+
+    /**
      * @var ProductMetadataInterface
      */
     protected $productMetadata;
+
+    /**
+     * @var ProductRepositoryInterface
+     */
+    protected $productRepository;
+
+    /**
+     * @var FlagFactory
+     */
+    protected $queueFlagFactory;
 
     /**
      * @var SourceCollectionFactory
@@ -64,37 +77,59 @@ class Inventory extends \MalibuCommerce\MConnect\Model\Queue implements Importab
     protected $sourceCollectionFactory;
 
     /**
-     * Inventory constructor.
-     *
-     * @param \Magento\Catalog\Api\ProductRepositoryInterface      $productRepository
-     * @param \MalibuCommerce\MConnect\Model\Navision\Inventory    $navInventory
-     * @param \MalibuCommerce\MConnect\Model\Config                $config
+     * @var SourceItemsProcessor
+     */
+    protected $sourceItemsProcessor;
+
+    /**
+     * @param Config $config
+     * @param DefaultSourceProviderInterface $defaultSourceProvider
+     * @param FlagFactory $queueFlagFactory
+     * @param NavisionInventory $navisionInventory
+     * @param IsSingleSourceModeInterface $isSingleSourceMode
+     * @param ProductMetadataInterface $productMetadata
+     * @param ProductRepositoryInterface $productRepository
+     * @param SourceCollectionFactory $sourceCollectionFactory
+     * @param SourceItemsProcessor $sourceItemsProcessor
+     * @param StockConfigurationInterface $stockConfiguration
      */
     public function __construct(
-        \Magento\Catalog\Api\ProductRepositoryInterface $productRepository,
-        \MalibuCommerce\MConnect\Model\Navision\Inventory $navInventory,
-        \MalibuCommerce\MConnect\Model\Config $config,
-        \MalibuCommerce\MConnect\Model\Queue\FlagFactory $queueFlagFactory,
+        Config $config,
         DefaultSourceProviderInterface $defaultSourceProvider,
+        FlagFactory $queueFlagFactory,
+        NavisionInventory $navisionInventory,
         IsSingleSourceModeInterface $isSingleSourceMode,
         ProductMetadataInterface $productMetadata,
+        ProductRepositoryInterface $productRepository,
         SourceCollectionFactory $sourceCollectionFactory,
-        StockConfigurationInterface $configuration
+        SourceItemsProcessor $sourceItemsProcessor,
+        StockConfigurationInterface $stockConfiguration
     ) {
-        $this->productRepository = $productRepository;
-        $this->navInventory = $navInventory;
         $this->config = $config;
-        $this->queueFlagFactory = $queueFlagFactory;
-        $this->configuration = $configuration;
         $this->defaultSourceProvider = $defaultSourceProvider;
         $this->isSingleSourceMode = $isSingleSourceMode;
+        $this->navisionInventory = $navisionInventory;
         $this->productMetadata = $productMetadata;
+        $this->productRepository = $productRepository;
+        $this->queueFlagFactory = $queueFlagFactory;
         $this->sourceCollectionFactory = $sourceCollectionFactory;
+        $this->sourceItemsProcessor = $sourceItemsProcessor;
+        $this->stockConfiguration = $stockConfiguration;
     }
 
+    /**
+     * Import action
+     *
+     * @param $websiteId
+     * @param int $navPageNumber
+     *
+     * @return bool|DataObject|Inventory
+     *
+     * @throws \Exception
+     */
     public function importAction($websiteId, $navPageNumber = 0)
     {
-        return $this->processMagentoImport($this->navInventory, $this, $websiteId, $navPageNumber);
+        return $this->processMagentoImport($this->navisionInventory, $this, $websiteId, $navPageNumber);
     }
 
     /**
@@ -102,96 +137,163 @@ class Inventory extends \MalibuCommerce\MConnect\Model\Queue implements Importab
      *
      * @param \SimpleXMLElement $data
      * @param int $websiteId
+     *
+     * @return void
      */
-    public function updateInventory($data, $websiteId = 0)
+    public function updateInventory($data, $websiteId = 0): void
     {
         $this->importEntity($data, $websiteId);
     }
 
-    public function importEntity(\SimpleXMLElement $data, $websiteId)
+    /**
+     * Import entity
+     *
+     * @param \SimpleXMLElement $data
+     * @param $websiteId
+     *
+     * @return bool
+     */
+    public function importEntity(\SimpleXMLElement $data, $websiteId): bool
     {
         $sku = (string)$data->nav_item_id;
         $sku = trim($sku);
         if (empty($sku)) {
             $this->messages .= 'SKU is missing' . PHP_EOL;
+
             return false;
         }
-
         try {
-            /** @var ProductInterface $product */
             $product = $this->productRepository->get($sku, true, null, true);
+            $priceIsUpdated = $this->updateProductPrice($product, $data, (int)$websiteId);
+            $qtyIsUpdated = $this->updateProductQty($product, $data, (int)$websiteId);
 
-            if (isset($data->quantity)) {
-                $quantity = (int)$data->quantity;
-            } elseif (isset($data->item_qty_on_hand)) {
-                $quantity = (int)$data->item_qty_on_hand;
-            } else {
-                $this->messages .= $sku . ': ' . 'QTY is missing' . PHP_EOL;
-                return false;
-            }
-
-            $stockStatus = (bool)$quantity;
-            $globalIsManageStock = $this->configuration->getManageStock();
-            $forcedInStock = $this->getConfig()->isInventoryInStockStatusMandatory($websiteId);
-
-            /** @var \Magento\CatalogInventory\Api\Data\StockItemInterface $stockItem */
-            $stockItem = $product->getExtensionAttributes()->getStockItem();
-            if (!$stockItem || !$stockItem->getId() || !$stockItem->getManageStock()
-                || ($stockItem->getUseConfigManageStock() && !$globalIsManageStock)
-            ) {
-                $this->messages .= $sku . ': skipped - stock for this product is not managed';
-
-                return false;
-            }
-
-            // Magento >= 2.3.x logic
-            if (version_compare($this->productMetadata->getVersion(), '2.3.0', '>=')
-                && !$this->isSingleSourceMode->execute()
-            ) {
-                $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
-                /** @var \MalibuCommerce\MConnect\Model\Queue\Inventory\SourceItemsProcessor $inventoryProcessor */
-                $inventoryProcessor = $objectManager->create(
-                    '\MalibuCommerce\MConnect\Model\Queue\Inventory\SourceItemsProcessor'
-                );
-                $sourceCodes = $this->getInventorySourceCodes($websiteId);
-                if (empty($sourceCodes)) {
-                    $this->messages .= $sku . ': skipped - inventory sources not found for website id #' . $websiteId;
-
-                    return false;
-                }
-                $sourceItemQty = [];
-                if (in_array($this->defaultSourceProvider->getCode(), $sourceCodes)) {
-                    $sourceItemQty[$this->defaultSourceProvider->getCode()] = $quantity;
-                }
-                foreach ($sourceCodes as $sourceCode) {
-                    $sourceCode = strtoupper($sourceCode);
-                    if (isset($data->$sourceCode)) {
-                        $sourceItemQty[$sourceCode] = (int)$data->$sourceCode;
-                    }
-                    $sourceCode = strtolower($sourceCode);
-                    if (isset($data->$sourceCode)) {
-                        $sourceItemQty[$sourceCode] = (int)$data->$sourceCode;
-                    }
-                }
-
-                $inventoryProcessor->process($product, $sourceItemQty, $stockStatus && $forcedInStock ? true : null);
-
-                return true;
-            }
-
-            // Magento <= 2.2.x logic
-            if ($stockStatus && $forcedInStock) {
-                $stockItem->setIsInStock($stockStatus);
-            }
-            $stockItem->setQty($quantity);
-            $stockItem->save();
-
-            $this->messages .= $sku . ': qty changed to ' . $quantity;
+            return $priceIsUpdated || $qtyIsUpdated;
         } catch (\Throwable $e) {
             $this->messages .= $sku . ': skipped - ' . $e->getMessage() . PHP_EOL;
 
             return false;
         }
+    }
+
+    /**
+     * Update product price
+     *
+     * @param ProductInterface $product
+     * @param \SimpleXMLElement $data
+     * @param int $website
+     *
+     * @return bool
+     *
+     * @throws CouldNotSaveException
+     * @throws InputException
+     * @throws StateException
+     */
+    protected function updateProductPrice(ProductInterface $product, \SimpleXMLElement $data, int $website): bool
+    {
+        if (!$this->config->isInventoryUpdatePrice($website)) {
+            return true;
+        }
+        if (!isset($data->unit_price)) {
+            $this->messages .= $product->getSku() . ': price update skipped - ' . 'unit price is missing' . PHP_EOL;
+
+            return false;
+        }
+        $price = (float)$data->unit_price;
+        if ($product->getPrice() == $price) {
+            $this->messages .= $product->getSku() . ': price not changed' . PHP_EOL;
+
+            return true;
+        }
+        $product->setPrice($price);
+        $this->productRepository->save($product);
+        $this->messages .= $product->getSku() . ': price changed to ' . $price . PHP_EOL;
+
+        return true;
+    }
+
+    /**
+     * Update product qty
+     *
+     * @param ProductInterface $product
+     * @param \SimpleXMLElement $data
+     * @param int $websiteId
+     *
+     * @return bool
+     *
+     * @throws NoSuchEntityException
+     */
+    protected function updateProductQty(ProductInterface $product, \SimpleXMLElement $data, int $websiteId): bool
+    {
+        if (isset($data->quantity)) {
+            $quantity = (int)$data->quantity;
+        } elseif (isset($data->item_qty_on_hand)) {
+            $quantity = (int)$data->item_qty_on_hand;
+        } else {
+            $this->messages .= $product->getSku() . ': qty update skipped - ' . 'QTY is missing' . PHP_EOL;
+
+            return false;
+        }
+
+        $stockStatus = (bool)$quantity;
+        $globalIsManageStock = $this->stockConfiguration->getManageStock();
+        $forcedInStock = $this->getConfig()->isInventoryInStockStatusMandatory($websiteId);
+
+        /** @var StockItemInterface $stockItem */
+        $stockItem = $product->getExtensionAttributes()->getStockItem();
+        if (!$stockItem || !$stockItem->getId() || !$stockItem->getManageStock()
+            || ($stockItem->getUseConfigManageStock() && !$globalIsManageStock)
+        ) {
+            $this->messages .= $product->getSku() . ': qty update skipped - stock for this product is not managed';
+
+            return false;
+        }
+
+        // Magento >= 2.3.x logic
+        if (version_compare($this->productMetadata->getVersion(), '2.3.0', '>=')
+            && !$this->isSingleSourceMode->execute()
+        ) {
+            $sourceCodes = $this->getInventorySourceCodes($websiteId);
+            if (empty($sourceCodes)) {
+                $this->messages .= sprintf(
+                    '%s: skipped - inventory sources not found for website id #%s' . PHP_EOL,
+                    $product->getSku(),
+                    $websiteId
+                );
+
+                return false;
+            }
+            $sourceItemQty = [];
+            if (in_array($this->defaultSourceProvider->getCode(), $sourceCodes)) {
+                $sourceItemQty[$this->defaultSourceProvider->getCode()] = $quantity;
+            }
+            foreach ($sourceCodes as $sourceCode) {
+                $sourceCode = strtoupper($sourceCode);
+                if (isset($data->$sourceCode)) {
+                    $sourceItemQty[$sourceCode] = (int)$data->$sourceCode;
+                }
+                $sourceCode = strtolower($sourceCode);
+                if (isset($data->$sourceCode)) {
+                    $sourceItemQty[$sourceCode] = (int)$data->$sourceCode;
+                }
+            }
+            $this->sourceItemsProcessor->process(
+                $product,
+                $sourceItemQty,
+                $stockStatus && $forcedInStock ? true : null
+            );
+            $this->messages .= $product->getSku() . ': qty changed in Multi Source Inventory';
+
+            return true;
+        }
+
+        // Magento <= 2.2.x logic
+        if ($stockStatus && $forcedInStock) {
+            $stockItem->setIsInStock($stockStatus);
+        }
+        $stockItem->setQty($quantity);
+        $stockItem->save();
+
+        $this->messages .= $product->getSku() . ': qty changed to ' . $quantity;
 
         return true;
     }
@@ -199,11 +301,11 @@ class Inventory extends \MalibuCommerce\MConnect\Model\Queue implements Importab
     /**
      * Get inventory source codes except default
      *
-     * @param $websiteId
+     * @param int $websiteId
      *
      * @return array
      */
-    protected function getInventorySourceCodes($websiteId)
+    protected function getInventorySourceCodes(int $websiteId)
     {
         /** @var SourceCollection $sourceCollection */
         $sourceCollection = $this->sourceCollectionFactory->create();
