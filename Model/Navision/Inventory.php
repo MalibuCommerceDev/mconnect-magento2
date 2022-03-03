61a24e37bad14123161466142649355394e313bb
Maksym Saveliev <maks@frendino.com>
2022-03-03 13:31:08 -0500

    Revert debug code

--- Model/Navision/Inventory.php
+++ Model/Navision/Inventory.php
@@ -15,24 +15,6 @@ class Inventory extends AbstractModel
             $parameters['last_updated'] = $lastUpdated;
         }
 
-        $data = '<?xml version="1.0" encoding="utf-8"?>
-<inventory_export_resp>
-<item_inventory>
-<nav_item_id>PSB8Z12-NN</nav_item_id>
-<quantity>0</quantity>
-<unit_price>6.25</unit_price>
-<src2>0</src2>
-<src3>110</src3>
-</item_inventory>
-<status>
-<current_date_time>2021-10-27T00:29:26Z</current_date_time>
-<record_count>1</record_count>
-<end_of_records>true</end_of_records>
-</status>
-</inventory_export_resp>';
-
-        return new \simpleXMLElement($data);
-
         return $this->_export('inventory_export', $parameters, $websiteId);
     }
 }
