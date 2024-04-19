badd4420f5c8d6b5ebe97816fd328288be40e029
Maksym Saveliev <maks@frendino.com>
2024-04-18 21:24:15 -0400

    Fixed DOMDocument::createCDATASection(): Passing null to parameter #1 ($data) of type string is deprecated in Model/SimpleXMLExtended.php on line 11

--- Model/SimpleXMLExtended.php
+++ Model/SimpleXMLExtended.php
@@ -6,6 +6,9 @@ class SimpleXMLExtended extends \SimpleXMLElement
 {
     public function addCData($value)
     {
+        if (empty($value)) {
+            $value = '';
+        }
         $node = dom_import_simplexml($this);
         $no = $node->ownerDocument;
         $node->appendChild($no->createCDATASection($value));
